fb96bf3039fe8372c6ae318e04d41e2d2e367ebc
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-10 07:53:17 -0300

    Adicionado o Projeto Ex_While para a elaboração dos exercícios.

---
+++ Ex_While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Senha");
+            int s = int.Parse(Console.ReadLine());
+
+            double Senha1 = Senha(s);
+
+            Console.WriteLine($"{Senha1}");
+        }
+
+        static int Senha(int senha)
+        {
+            while (senha != 2002)
+            {
+                Console.WriteLine("A senha informada é incorreta, digite novamente");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha Correta!");
+            return senha;
+        }
+    }
+}
