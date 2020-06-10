769fe718499032792e88d085ea360f6b6780bd4f
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-10 09:23:08 -0300

    Adicionado o namespace While2

--- Ex_While/Program.cs
+++ Ex_While/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Ex_While
+/*namespace Ex_While
 {
     class Program
     {
@@ -25,4 +25,56 @@ namespace Ex_While
             return senha;
         }
     }
+}*/
+
+#region EX2
+namespace Ex_While2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois valores separados por um ' '");
+            string[] var = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(var[0]);
+            int y = int.Parse(var[1]);
+
+            double Coord = Coordenadas(x, y);
+
+            Console.WriteLine(Coord);
+        }
+
+        static int Coordenadas(int x, int y)
+        {
+            while (x != 0 && y != 0)
+            {
+                
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("A cordenada pertence ao 1º Quadrante");
+                }
+                else if (x< 0 && y > 0)
+                {
+                    Console.WriteLine("Cordenada 2º Quadrante");
+                }
+                else if (x < 0 && y< 0)
+                {
+                    Console.WriteLine("Pertence ao 3º Quadrante");
+                }
+                else
+                {
+                    Console.WriteLine("Pertence ao 4º Quadrante");
+                }
+                Console.WriteLine("Digite novas coordenadas");
+                string[] coord = Console.ReadLine().Split(' ');
+                x = int.Parse(coord[0]);
+                y = int.Parse(coord[1]);
+            }
+            Console.WriteLine("Coordenada errada");
+            return 0;
+        }
+    }
+
 }
+#endregion
