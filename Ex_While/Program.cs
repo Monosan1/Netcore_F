cccffda6f24c13e98cf3da3b130815210084540f
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-10 10:03:25 -0300

    Foi Adicionado o namespace While3.

--- Ex_While/Program.cs
+++ Ex_While/Program.cs
@@ -28,7 +28,7 @@
 }*/
 
 #region EX2
-namespace Ex_While2
+/*namespace Ex_While2
 {
     class Program
     {
@@ -49,16 +49,16 @@ namespace Ex_While2
         {
             while (x != 0 && y != 0)
             {
-                
+
                 if (x > 0 && y > 0)
                 {
                     Console.WriteLine("A cordenada pertence ao 1º Quadrante");
                 }
-                else if (x< 0 && y > 0)
+                else if (x < 0 && y > 0)
                 {
                     Console.WriteLine("Cordenada 2º Quadrante");
                 }
-                else if (x < 0 && y< 0)
+                else if (x < 0 && y < 0)
                 {
                     Console.WriteLine("Pertence ao 3º Quadrante");
                 }
@@ -76,5 +76,59 @@ namespace Ex_While2
         }
     }
 
-}
+}*/
 #endregion
+
+namespace While3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alcool 1 - Gasolina 2 - 3 Diessel - 4 Fim");
+            Console.WriteLine("Digite um número correspondente a sua necessidade");
+            int comb = int.Parse(Console.ReadLine());
+
+            double Combs = combustivel(comb);
+
+            Console.WriteLine(Combs);
+        }
+
+        static int combustivel(int comb)
+        {
+            int gasolina = 0;
+            int alcool = 0;
+            int diessel = 0;
+
+            while (comb != 4)
+            {
+                if (comb == 1)
+                {
+                    gasolina = gasolina + 1;
+                }
+                else if (comb == 2)
+                {
+                    alcool = alcool + 1;
+                }
+                else if (comb == 3)
+                {
+                    diessel = diessel + 1;
+                }
+                
+               
+                    Console.WriteLine("Digite um número correto");
+                    comb = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine($"Alcool: {alcool}");
+            Console.WriteLine($"Gasolina: {gasolina}");
+            Console.WriteLine($"Diesel: {diessel}");
+
+
+            return 0;
+        }
+    }
+
+}
+
