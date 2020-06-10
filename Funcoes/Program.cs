8271b56b54bb9607f683b4c74619219115d8a46b
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-10 00:08:29 -0300

    Readicionando a Função Maior

--- Funcoes/Program.cs
+++ Funcoes/Program.cs
@@ -15,12 +15,26 @@ namespace Funcoes
 
             double resultado = Maior(n1, n2, n3);
 
-            Console.WriteLine($"Mairo ={resultado}");
+            Console.WriteLine($"Maior ={resultado}");
         }
 
         static int Maior(int a, int b, int c)
         {
+            int m;
+            if (a>b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
 
+            return m;
         }
 
     }
