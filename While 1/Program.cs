e453bcf2659ecb743052af85bea26825a4d20fa8
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-10 00:32:49 -0300

    Adicionado o looping While.

--- While 1/Program.cs
+++ While 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace While_1
 {
@@ -6,7 +7,20 @@ namespace While_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite um número");
+            double num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+           
+            while (num1 >= 0.0)
+            {
+                double raiz = Math.Sqrt(num1);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite Outro número");
+                num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Número negativo!");
+            
         }
     }
 }
